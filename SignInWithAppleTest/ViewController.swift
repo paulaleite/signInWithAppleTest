c0dd107b4e90493f5ce93a0d8a6486987f9677c0
Paula Leite <paula.t.leite@icloud.com>
2020-04-02 14:52:57 -0300

    Added the delegate extension to the viewController

--- SignInWithAppleTest/ViewController.swift
+++ SignInWithAppleTest/ViewController.swift
@@ -16,6 +16,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+}
 
+extension ViewController: ASAuthorizationControllerDelegate {
+    
 }
 
